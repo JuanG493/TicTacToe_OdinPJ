c732cb90336b700f9942ffe8d69d5deea9178081
juan493 <juanguarin493@gmail.com>
2023-02-13 17:47:36 +0100

    main functionalities  working, need to be stylish and debug some functionalities of buttons

--- TicTacToc.js
+++ TicTacToc.js
@@ -22,16 +22,17 @@ const gameBoard = (() => {
          boardTarget.appendChild(divElment);
       }
    }
-   return { callDomy }
+   return { gridBoard, callDomy }
 })();
 
 
-function createPerson(name, pointer) {
+function createPerson(name, pointer, type = 'person') {
    return {
       name,
       ubications: [],
       points: 0,
-      pointer: pointer
+      pointer: pointer,
+      type: type
    }
 }
 
@@ -43,7 +44,7 @@ function makingPersons(num, ind) {
       players.push(partial)
    }
    if (ind == 'pc') {
-      let partial = createPerson('PC-DEXTRO', '@')
+      let partial = createPerson('PC-DEXTRO', '$', 'PC')
       players.push(partial)
 
    }
@@ -51,8 +52,8 @@ function makingPersons(num, ind) {
 
 const deletePlayers = (() => {
    let delContentPj = () => {
-      players[0].ubications = [];
-      players[1].ubications = [];
+      players[0].ubications.length = 0;
+      players[1].ubications.length = 0;
    }
    let deleteboard = () => {
       let allDivs = document.querySelectorAll('.zone');
@@ -68,7 +69,7 @@ const deletePlayers = (() => {
       ctrlFlow.zone();
    })
    // setNamePoints.drawPlayerName();
-   return {delContentPj, deleteboard, deleteDiv }
+   return { delContentPj, deleteboard, deleteDiv }
 })();
 
 
@@ -136,87 +137,246 @@ const selectorEvents = (() => {
          setNamePoints.drawPlayerName();
       }
    })
-
+   
    let play = document.querySelector('#play')
    play.addEventListener('click', () => {
-      // becouse I have to delete the two Object by default
+      // because I have to delete the two Object by default
       players.splice(0, 2)
       starterGame()
    })
-
+   
 })()
-
 function makeResult(msj) {
    let results = document.querySelector('#results');
    results.innerHTML = `${msj}`;
    setTimeout(() => {
       results.innerHTML = ''
+      
+   }, 4000);
+}
 
-   }, 2000);
+// here because I nedd to elminate the function that handle the events
+
+function clickDivElm() {
+   let element = this
+   let coordinate = this.dataset.key;
+   ctrlFlow.assignTurn(coordinate, element)
 }
 
 
-//is here becouse I nedd to operate after the button play is clicked//
 const ctrlFlow = (() => {
+
    const zone = () => {
       let grid = document.querySelectorAll('.zone');
       grid.forEach((element) => {
-         // element.innerHTML = '';
-         element.addEventListener('click', () => {
-            // event.stopPropagation()
-            let coordinate = element.getAttribute("data-key");
-            assignTurn(coordinate, element)
-         }, { once: true })
+         element.addEventListener('click', clickDivElm, { once: true });
       })
    }
-   return { zone }
 
-
-   function assignTurn(ubication, divElm) {
+   const assignTurn = (ubication, divElm) => {
       let selctPj1 = players[0].ubications;
-      let selctPj2 = players[1].ubications;
+      let selctPc = players[1].ubications;
 
-      if (selctPj1.length == selctPj2.length) {
+      if (players[1].type == 'PC') {
          selctPj1.push(ubication)
          divElm.innerHTML = players[0].pointer
-         // si lenght mayor de tres enviar
-         if (selctPj1.length >= 3) {
-            checkWinner(selctPj1, players[0])
+
+         let testing = checkWinner(selctPj1, players[0])
+ 
+         if (!testing){
+            let nextMove = minmaxT.tranformBoard()
+            selctPc.push(nextMove);
+            let where = document.querySelector(`div[data-key="${nextMove}"]`);
+   
+            where.innerHTML = players[1].pointer;
+            where.removeEventListener('click', clickDivElm, { once: true });
+   
+            if (selctPc.length >= 3) {
+               checkWinner(selctPc, players[1])
+            }
          }
 
       } else {
-         selctPj2.push(ubication);
-         divElm.innerHTML = players[1].pointer
-         // si lenght mayor de tres enviar
-         if (selctPj2.length >= 3) {
-            checkWinner(selctPj2, players[1])
-         }
-      }
 
-      function checkWinner(choosePj, pj) {
+         if (selctPj1.length == selctPc.length) {
 
-         let sorting = choosePj.sort().join('');
-         let cheker = new RegExp(/([A][1-3]){3}|([B][1-3]){3}|([c][1-3]){3}|(?:[a-c]1\w*?){3}|(?:[a-c]2\w*?){3}|(?:[a-c]3\w*?){3}|a1\w*?b2\w*?c3|a3\w*?b2\w*?c1/, 'gi')
-         let winner = cheker.test(sorting);
-         if (winner) {
-            makeResult(`WINNER ${pj.name}`)
+            selctPj1.push(ubication)
+            divElm.innerHTML = players[0].pointer
 
-            pj.points += 1
-            setNamePoints.drawPlayerName()
-            deletePlayers.delContentPj();
-            deletePlayers.deleteDiv();
-         }
-         if (!winner && choosePj.length == 5) {
-            makeResult('TIE');
-            deletePlayers.delContentPj();
-            deletePlayers.deleteDiv();
+            // si lenght mayor de tres enviar
+            if (selctPj1.length >= 3) {
+               checkWinner(selctPj1, players[0]);
+            }
 
+         } else {
+            selctPc.push(ubication);
+            divElm.innerHTML = players[1].pointer
+            // si lenght mayor de tres enviar
+            if (selctPc.length >= 3) {
+               checkWinner(selctPc, players[1]);
+            }
 
          }
       }
    }
+
+
+   const checkWinner = (chooseOfPj, player) => {
+      let winner = minmaxT.evaluate(chooseOfPj.sort(), []);
+      if (winner == -10) {
+         makeResult(`WINNER ${player.name}`)
+
+         player.points += 1
+         setNamePoints.drawPlayerName()
+         deletePlayers.delContentPj();
+         deletePlayers.deleteDiv();
+         return true
+      }
+      else if (winner == 0 && chooseOfPj.length == 5) {
+         makeResult('TIE');
+         deletePlayers.delContentPj();
+         deletePlayers.deleteDiv();
+         return true
+
+      }
+      else {
+         return false
+      }
+   }
+   return { zone, assignTurn, checkWinner }
 })();
 
+
+
+const minmaxT = (() => {
+
+   function tranformBoard() {
+      let board = {}
+      let allElments = document.querySelectorAll('.zone');
+
+      allElments.forEach(elm => {
+         let totem = elm.dataset.key
+         if (elm.innerHTML == '') {
+            board[totem] = '_';
+         }
+         else if (elm.innerHTML == '$') {
+            board[totem] = players[1].pointer
+         }
+         else {
+            board[totem] = players[0].pointer
+         }
+      })
+      return intermediaria(board);
+   }
+
+   function intermediaria(board) {
+      let bestVal = -1000
+      let bestMove = '';
+
+      for (let i in board) {
+         if (board[i] == '_') {
+            board[i] = players[1].pointer;
+            let moveVal = minmaxT(board, 0, false);
+            board[i] = '_'
+
+            if (moveVal > bestVal) {
+               bestMove = i;
+               bestVal = moveVal
+            }
+         }
+      }
+      return bestMove
+   };
+
+   function minmaxT(board, depth, isMaxTurn) {
+      let score = evaluation(board)
+      if (score == 10) {
+         return score;
+      }
+      if (score == -10) {
+         return score;
+      }
+      if (movesleft(board) == false) {
+         return 0
+      }
+      if (isMaxTurn) {
+         let best = -1000
+
+         for (let key in board) {
+            if (board[key] == '_') {
+               board[key] = players[1].pointer
+               best = Math.max(best, minmaxT(board, depth + 1, !isMaxTurn));
+               board[key] = '_'
+            }
+         }
+         return best
+      } else {
+         let best = 1000;
+         for (let key in board) {
+            if (board[key] == '_') {
+               board[key] = players[0].pointer
+               best = Math.min(best, minmaxT(board, depth + 1, !isMaxTurn));
+               board[key] = '_'
+            }
+         }
+         return best
+      }
+   }
+
+   function movesleft(board) {
+      for (let key in board) {
+         if (board[key] == '_') {
+            return true
+         }
+      }
+      return false
+   }
+
+   function evaluation(board) {
+      let opponetTeam = [];
+      let pcTeam = [];
+
+      for (let key in board) {
+         if (board[key] == 'X') {
+            opponetTeam.push(key)
+         }
+         if (board[key] == '$') {
+            pcTeam.push(key)
+         }
+      }
+
+      if (opponetTeam.length >= 3 || pcTeam.length >= 3) {
+         return evaluate(opponetTeam, pcTeam)
+
+      } else {
+         return 0
+      }
+   }
+   function evaluate(teamX, teamPc) {
+
+      let patterns = {
+         rows: /((?:[A][1-3]){3}|(?:[B][1-3]){3}|(?:[c][1-3]){3})/gi,
+         columns: /((?:[a-c]1\w*?){3}|(?:[a-c]2\w*?){3}|(?:[a-c]3\w*?){3})/gi,
+         diagonals: /(a1\w*?b2\w*?c3|a3\w*?b2\w*?c1)/gi
+
+      }
+      let strOponent = teamX.join('');
+      let strPlayerPc = teamPc.join('');
+
+      for (let key in patterns) {
+
+         if (patterns[key].test(strOponent)) {
+            return -10
+         }
+         if (patterns[key].test(strPlayerPc)) {
+            return 10
+         }
+      }
+      return 0
+   }
+   return { intermediaria, tranformBoard, evaluate }
+
+})();
 
 
 
