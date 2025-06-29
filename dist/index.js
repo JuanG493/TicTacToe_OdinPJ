86cb4eac750e66a8b95b73a61ec85bfed3f274bf
juanG493 <juanguarin493@gmail.com>
2025-06-30 01:27:30 +0200

    evaluation working for all positions

---
+++ dist/index.js
@@ -0,0 +1,38 @@
+const checkEquality = (board, marker) => {
+    return board[0] === marker && board[1] === marker && board[2] === marker;
+};
+export const evaluator = (board, marker) => {
+    marker = marker.toLowerCase();
+    let winner = false;
+    let columns = [];
+    for (let row = 0; row < board.length && !winner; row++) {
+        let target = board[row];
+        //check the complete row
+        winner = checkEquality(target, marker);
+        for (let col = 0; col < target.length && !winner; col++) {
+            if (!columns[col]) {
+                columns[col] = ["", "", ""];
+            }
+            columns[col][row] = target[col];
+        }
+    }
+    //check the columns
+    if (!winner) {
+        for (let row = 0; row < columns.length && !winner; row++) {
+            winner = checkEquality(columns[row], marker);
+        }
+    }
+    //check the diagonales
+    if (!winner) {
+        winner =
+            checkEquality([board[0][0], board[1][1], board[2][2]], marker) ||
+                checkEquality([board[0][2], board[1][1], board[2][0]], marker);
+    }
+    return winner;
+};
+// // evaluator(winBoard, "x");
+// evaluator([
+//     ["x", "_", "_"],
+//     ["_", "x", "_"],
+//     ["_", "_", "x"]
+// ], "x")
---
+++ js/board.test.js
@@ -0,0 +1,75 @@
+import { describe, expect, test } from 'vitest';
+import { evaluator } from './index.js';
+
+test('base test', () => {
+    expect(evaluator([
+        ["_", "_", "_"],
+        ["_", "_", "_"],
+        ["_", "_", "_"]
+    ], "x")).toBeFalsy();
+})
+
+describe('test for rows', () => {
+    test("win in first row", () => {
+        expect(evaluator([
+            ["x", "x", "x"],
+            ["_", "_", "_"],
+            ["_", "_", "_"]
+        ], "x")).toBeTruthy();
+    })
+    test("win in second row", () => {
+        expect(evaluator([
+            ["_", "_", "_"],
+            ["x", "x", "x"],
+            ["_", "_", "_"]
+        ], "x")).toBeTruthy();
+    })
+    test("win in third row", () => {
+        expect(evaluator([
+            ["_", "_", "_"],
+            ["_", "_", "_"],
+            ["x", "x", "x"]
+        ], "x")).toBeTruthy();
+    })
+})
+
+describe('test for columns', () => {
+    test("win in first col", () => {
+        expect(evaluator([
+            ["x", "_", "_"],
+            ["x", "_", "_"],
+            ["x", "_", "_"]
+        ], "x")).toBeTruthy();
+    })
+    test("win in second col", () => {
+        expect(evaluator([
+            ["_", "x", "_"],
+            ["_", "x", "_"],
+            ["_", "x", "_"]
+        ], "x")).toBeTruthy();
+    })
+    test("win in third col", () => {
+        expect(evaluator([
+            ["_", "_", "x"],
+            ["_", "_", "x"],
+            ["_", "_", "x"]
+        ], "x")).toBeTruthy();
+    })
+})
+
+describe('test for diagonales', () => {
+    test("win in first dig", () => {
+        expect(evaluator([
+            ["x", "_", "_"],
+            ["_", "x", "_"],
+            ["_", "_", "x"]
+        ], "x")).toBeTruthy();
+    })
+    test("win in second dig", () => {
+        expect(evaluator([
+            ["_", "_", "x"],
+            ["_", "x", "_"],
+            ["x", "_", "_"]
+        ], "x")).toBeTruthy();
+    })
+})
