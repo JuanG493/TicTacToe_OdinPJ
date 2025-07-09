d1e212322d24cdf1eb0515f099fdc8669645449e
juanG493 <juanguarin493@gmail.com>
2025-07-09 21:01:43 +0200

    some anotations

--- js/minMax.ts
+++ js/minMax.ts
@@ -25,38 +25,30 @@ function evaluate(board: board[], player: string, opponent: string): number {
     return 0;
 }
 
+
+// algo from  https://www.geeksforgeeks.org/minimax-algorithm-in-game-theory-set-3-tic-tac-toe-ai-finding-optimal-move/
 function minimax(board: board[], depth: number, isMax: boolean, player: string, opponent: string) {
     let score = evaluate(board, player, opponent);
 
-    // If Maximizer has won the game
-    // return his/her evaluated score
     if (score == 10)
         return score;
 
-    // If Minimizer has won the game
-    // return his/her evaluated score
     if (score == -10)
         return score;
 
-    // If there are no more moves and
-    // no winner then it is a tie
     if (isMovesLeft(board) == false)
         return 0;
 
-    // If this maximizer's move
     if (isMax) {
         let best = -1000;
 
         // Traverse all cells
         for (let i = 0; i < 3; i++) {
             for (let j = 0; j < 3; j++) {
-
                 // Check if cell is empty
                 if (board[i][j] == '_') {
-
                     // Make the move
                     board[i][j] = player;
-
                     // Call minimax recursively
                     // and choose the maximum value
                     best = Math.max(best, minimax(board, depth + 1, !isMax, player, opponent));
@@ -68,11 +60,9 @@ function minimax(board: board[], depth: number, isMax: boolean, player: string, 
         }
         return best;
     }
-
     // If this minimizer's move
     else {
         let best = 1000;
-
         // Traverse all cells
         for (let i = 0; i < 3; i++) {
             for (let j = 0; j < 3; j++) {
@@ -97,34 +87,23 @@ function minimax(board: board[], depth: number, isMax: boolean, player: string, 
     }
 }
 
-// This will return the best possible
-// move for the player
 function findBestMove(board: board[], player: string, opponent: string): Move {
     let bestVal = -1000;
     let bestMove = new Move();
     bestMove.row = -1;
     bestMove.col = -1;
 
-    // Traverse all cells, evaluate
-    // minimax function for all empty
-    // cells. And return the cell
-    // with optimal value.
     for (let i = 0; i < 3; i++) {
         for (let j = 0; j < 3; j++) {
-
             // Check if cell is empty
             if (board[i][j] == '_') {
-
                 // Make the move
                 board[i][j] = player;
-
                 // compute evaluation function
                 // for this move.
                 let moveVal = minimax(board, 0, false, player, opponent);
-
                 // Undo the move
                 board[i][j] = '_';
-
                 // If the value of the current move
                 // is more than the best value, then
                 // update best
